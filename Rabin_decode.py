7abff9ad44672f2b34cf8862c2bbdc0a03e2c430
Taiyaki <45515964+taiyaki8926@users.noreply.github.com>
2019-08-26 01:34:05 +0900

    Update Rabin_decode.py

--- Rabin_decode.py
+++ Rabin_decode.py
@@ -2,6 +2,7 @@ def legendre_symbol(a, p):
     ls = pow(a, (p - 1) // 2, p)
     return -1 if ls == p - 1 else ls
 
+
 def modular_sqrt(a, p):
     if legendre_symbol(a, p) != 1:
         return 0
@@ -10,7 +11,7 @@ def modular_sqrt(a, p):
     elif p == 2:
         return 0
     elif p % 4 == 3:
-        return pow(a, (p + 1) / 4, p)
+        return pow(a, (p + 1) // 4, p)
     
     s = p - 1
     e = 0
@@ -47,19 +48,14 @@ def egcd(a, b):
     else:
         g, y, x = egcd(b % a, a)
         return (g, x - (b // a) * y, y)
+ 
 
-
-c = 196353764385075548782571270052469419021844481625366305056739966550926484027148967165867708531585849658610359148759560853
-p = 5411451825594838998340467286736301586172550389366579819551237
-q = 5190863621109915362542582192103708448607732254433829935869841
-
-def dec(t):
-    mp = modular_sqrt(t, p)
-    mq = modular_sqrt(t, q)
+# n = p * q
+def decode(c, p, q):
+    mp = modular_sqrt(c, p)
+    mq = modular_sqrt(c, q)
     _, yp, yq = egcd(p, q)
-    r = (yp * p * mq + yq * q * mp) % n
-    s = (yp * p * mq - yq * q * mp) % n
+    r = (yp * p * mq + yq * q * mp) % (p * q)
+    s = (yp * p * mq - yq * q * mp) % (p * q)
     ms = [r, s, n - r, n - s]
     return ms
-
-pow(dec(c)[], 2, p*q) == c
